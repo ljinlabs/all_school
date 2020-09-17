5a6e18011df9b118205d6dbf026f6120da6bde6a
J Lee <ljin0906@gmail.com>
2020-09-17 10:26:22 -0500

    add admin fufor course and sem models

--- courselist/admin.py
+++ courselist/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
-
+from courselist.models import Course, Semester
 # Register your models here.
+
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ('course_title','course_last_updated')
+    pass
+
+class SemesterAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(Course, CourseAdmin)
+admin.site.register(Semester, SemesterAdmin)
